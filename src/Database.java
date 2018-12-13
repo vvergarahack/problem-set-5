2fa21fc10f3e6edd475fb395398d0661d4a63b7b
vvergarahack <veronica.vergara@njwg.cap.gov>
2018-12-12 22:12:32 -0500

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -1,119 +1,104 @@
-import java.io.BufferedReader;
+import java.util.Arrays;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.IOException;
+/**
+ * This class will serve as the intermediary between our ATM program and
+ * the database of BankAccounts. It'll be responsible for fetching accounts
+ * when users try to login, as well as updating those accounts after any
+ * changes are made.
+ */
 
 public class Database {
-	
+	/*Scanner in = new Scanner(System.in);
+	private static long genActNum = 100000001L;
+	private long actNum;
+	private double balance;
+	private User user;
+	*/
 	private String path;
-	private String[] accounts;
-	
-	public Database(String path) throws FileNotFoundException, IOException {
+	private String[] acts;
+	public Database(String path) throws FileNotFoundException, IOException{
 		this.path = path;
-		this.accounts = getAllAccounts();
+		this.acts = getAllActs();
 	}
 	
-	/**
-	 * Initializes the database with all accounts.
-	 * 
-	 * @return an array of all accounts
-	 */
+	///////////////////////GET ALL ACCOUNTS//////////////////////////
 	
-	public String[] getAllAccounts() throws FileNotFoundException, IOException {
-		int count = 0;
-		String[] accounts = new String[10];
-		
-		FileReader altered = null;
-		InputStreamReader original = null;
+	public String[] getAllActs() throws FileNotFoundException, IOException{
+		int i = 0;
+		String[] acts = new String[10];
+		FileReader alt = null;
+		InputStreamReader org = null;
 		try {
-			altered = new FileReader(System.getProperty("user.dir") + File.separator + path);			
-		} catch (FileNotFoundException e) {
-			original = new InputStreamReader(getClass().getResourceAsStream(path));
+			alt = new FileReader(System.getProperty("user.dir") + File.separator + path);
+		}
+		catch(FileNotFoundException e){
+			org = new InputStreamReader(getClass().getResourceAsStream(path));
 		}
 		
-		try (BufferedReader br = new BufferedReader(original != null ? original : altered)) {
+		try(BufferedReader br = new BufferedReader(org!=null ? org : alt)) {
 			String line;
-			
-			while ((line = br.readLine()) != null) {
-				if (count >= accounts.length) {
-					accounts = Arrays.copyOf(accounts, accounts.length + 10);
+			while((line = br.readLine())!=null) {
+				if(i>= acts.length) {
+					acts = Arrays.copyOf(acts,  acts.length + 10);
 				}
-				accounts[count++] = line;
+				acts[i++] = line;
 			}
 		}
-		
-		return Arrays.copyOf(accounts, count);
+		return Arrays.copyOf(acts, i);
 	}
 	
-	/**
-	 * Retrieves an account by account number.
-	 * 
-	 * @param accountNumber the acocunt number of the account to retrieve
-	 * @return a BankAccount
-	 */
+	///////////////////////READ ACCOUNT//////////////////////////
 	
-	public BankAccount getAccount(long accountNumber) {
-		for (String account : accounts) {
-			if (account.startsWith(String.valueOf(accountNumber)) && account.endsWith("Y")) {
-				return new BankAccount(account);
+	public String readAct (long actNum) {
+		for(String act : acts) {
+			if(act.startsWith(String.valueOf(actNum)) && act.endsWith("Y")) {
+				return act;
 			}
 		}
-		
 		return null;
+		//read
 	}
 	
-	/**
-	 * Updates a BankAccount.
-	 * 
-	 * @param account the primary account being updated
-	 * @param destination the secondary account being updated
-	 * @throws IOException 
-	 */
+	///////////////////////WRITE ACCOUNT//////////////////////////
 	
-	public void updateAccount(BankAccount account, BankAccount destination) throws IOException {
-		boolean newAccount = true;
-		
-		for (int i = 0; i < accounts.length; i++) {			
-			if (accounts[i].startsWith(String.valueOf(account.getAccountNumber()))) {
-				accounts[i] = account.toString();
-				newAccount = false;
+	public void writeAct (BankAccount act, BankAccount dest) throws IOException {
+		boolean newAct = true;
+		for(int i = 0; i< acts.length; i++) {
+			if(acts[i].startsWith(String.valueOf(act.getActNum()))){
+				acts[i] = act.toString();
+				newAct = false;
 			}
-			
-			if (destination != null) {
-				if (accounts[i].startsWith(String.valueOf(destination.getAccountNumber()))) {
-					accounts[i] = destination.toString();
+			if(dest != null){
+				if(acts[i].startsWith(String.valueOf(dest.getActNum()))) {
+					acts[i]=act.toString();
 				}
 			}
 		}
-		
-		if (newAccount) {
-			accounts = Arrays.copyOf(accounts, accounts.length + 1);
-			accounts[accounts.length - 1] = account.toString();
+		if(newAct) {
+			acts = Arrays.copyOf(acts, acts.length+1);
+			acts[acts.length-1] = act.toString();
 		}
-		
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(System.getProperty("user.dir") + File.separator + path))) {
-			for (String acct : accounts) {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(System.getProperty("user.dir")+File.separator+path))) {
+			for(String acct:acts) {
 				bw.write(acct);
 				bw.newLine();
 			}
 		}
 	}
 	
-	/**
-	 * Retrieves the largest account number in the database.
-	 * 
-	 * @return the largest account number
-	 */
+	///////////////////////MAX ACCOUNT NUM//////////////////////////
 	
 	public long getMaxAccountNumber() {
 		long max = -1L;
 		
-		for (String account : accounts) {
+		for (String account : acts) {
 			long accountNumber = Long.parseLong(account.substring(0, 9));
 			
 			if (accountNumber > max) {
@@ -123,4 +108,5 @@ public class Database {
 		
 		return max;
 	}
+	
 }
